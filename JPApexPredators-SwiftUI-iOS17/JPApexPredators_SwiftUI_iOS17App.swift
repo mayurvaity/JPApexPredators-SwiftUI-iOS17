9cafec8848e841f4a60df567579ca77fecf5217c
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-29 20:50:33 +0530

    Initial Commit.

---
+++ JPApexPredators-SwiftUI-iOS17/JPApexPredators_SwiftUI_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators_SwiftUI_iOS17App.swift
+//  JPApexPredators-SwiftUI-iOS17
+//
+//  Created by Mayur Vaity on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators_SwiftUI_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
